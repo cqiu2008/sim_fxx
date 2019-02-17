f3220f2a2bb72dc76c48a6ceebfd04c0714ee1db
cqiu <cqiu2008@hotmail.com>
2019-02-17 23:40:36 +0800

    add lzw reserver.v

--- rtl/lzw_backward_byte_reverse.v
+++ rtl/lzw_backward_byte_reverse.v
@@ -32,7 +32,6 @@ reg                S_dictionary_recv_data_en=1'b0 ;
 reg                S_reverse_byte_flag      =1'b0 ; 
 reg         [ 4:0] S_reverse_byte_num       =5'h0 ; 
 reg                S_reverse_byte_num_wren  =1'b0 ; 
-reg                S_clk_div2               =1'b0 ;
 
 reg         [ 4:0] S_para_fifo_wdata        =5'h0 ;
 reg                S_para_fifo_wren         =1'b0 ;
@@ -43,6 +42,7 @@ wire               S_para_fifo_empty              ;
 wire        [ 9:0] S_para_fifo_data_count         ;
 reg         [ 8:0] S_data_fifo_wdata        =9'h0 ;
 reg                S_data_fifo_wren         =1'b0 ;
+reg                S_data_fifo_rd_req       =1'b0 ;
 reg                S_data_fifo_rden         =1'b0 ;
 wire        [ 8:0] S_data_fifo_dout               ;
 wire               S_data_fifo_full               ;
@@ -52,13 +52,38 @@ wire        [ 9:0] S_data_fifo_data_count         ;
 wire        [4:0]  S_rd_rev_num                   ;
 wire               S_rd_rev_flg                   ;
 wire        [7:0]  S_rd_rev_data                  ;
+
 reg         [4:0]  S_rd_rev_cnt             =5'h0 ;
-reg                S_data_fifo_rden_d1      =1'b0 ;
-reg                S_data_fifo_rden_d2      =1'b0 ;
-reg                S_data_fifo_rden_d3      =1'b0 ;
+reg         [4:0]  S_rd_rev_cnt_d1          =5'h0 ;
+reg                S_rd_rev_pos             =1'b0 ;
+reg                S_rd_rev_pos_d1          =1'b0 ;
+reg                S_rd_rev_valid           =1'b0 ;
+reg                S_rd_rev_end             =1'b0 ;
+reg                S_rd_rev_end_d1          =1'b0 ;
+reg                S_rd_rev_end_d2          =1'b0 ;
+reg                S_rd_rev_data_en         =1'b0 ;
+reg                S_rd_rev_data_en_d1      =1'b0 ;
+reg                S_rd_rev_data_en_d2      =1'b0 ;
+reg                S_rd_rev_data_en_d3      =1'b0 ;
+reg                S_rd_rev_data_en_d4      =1'b0 ;
+reg         [7:0]  S_rd_rev_data_d1         =8'h0 ;
+reg         [7:0]  S_rd_rev_data_d2         =8'h0 ;
+reg         [7:0]  S_rd_rev_data_d3         =8'h0 ;
+reg         [7:0]  S_rd_rev_data_d4         =8'h0 ;
+reg         [9:0]  S_ram_base_addr          =10'h0;
 reg         [9:0]  S_ram_wr_addr            =10'h0;
 reg                S_ram_wr_en              =1'b0 ;
 reg         [7:0]  S_ram_wr_data            =8'h0 ;
+reg                S_ram_rd_ready           =1'b0 ;
+reg                S_ram_rd_en              =1'b0 ;
+reg                S_ram_rd_en_d1           =1'b0 ;
+reg         [9:0]  S_ram_rd_addr            =10'h0;
+wire        [7:0]  S_ram_rd_data                  ;
+reg                S_state_clr              =1'b0 ;
+reg        [15:0]  S_data_fifo_wr_cnt       =16'h0;
+reg        [15:0]  S_data_fifo_rd_cnt       =16'h0;
+reg        [15:0]  S_para_fifo_wr_cnt       =16'h0;
+reg        [15:0]  S_para_fifo_rd_cnt       =16'h0;
 
 always @(posedge I_sys_clk)
 begin
@@ -131,14 +156,21 @@ assign  S_rd_rev_data  = S_data_fifo_dout[7:0];
 
 always @(posedge I_sys_clk)
 begin
-    S_clk_div2 <= ~S_clk_div2;
+    if((S_data_fifo_data_count >= 10'h20) && !S_para_fifo_empty)
+    begin
+        S_data_fifo_rd_req <= 1'b1;
+    end
 end
 
 always @(posedge I_sys_clk)
 begin
-    if(S_clk_div2 && (S_data_fifo_data_count > 10'hff))
+    if(S_data_fifo_empty)
     begin
-        S_data_fifo_rden <= 1'b1;
+        S_data_fifo_rden <= 1'b0;
+    end
+    else if(S_data_fifo_rd_req)
+    begin
+        S_data_fifo_rden <= ~S_data_fifo_rden;
     end
     else
     begin
@@ -147,27 +179,13 @@ begin
 end
 
 always @(posedge I_sys_clk)
-begin
-    if(S_clk_div2)
-    begin
-        if(S_rd_rev_flg && (S_rd_rev_cnt < S_rd_rev_num))
-        begin
-            S_rd_rev_cnt <= S_rd_rev_cnt + 5'h1;
-        end
-        else if(S_rd_rev_flg && (S_rd_rev_cnt == S_rd_rev_num) && (S_rd_rev_cnt !=5'h0))
-        begin
-            S_rd_rev_cnt <= 5'h1;
-        end
-        else
-        begin
-            S_rd_rev_cnt <= 5'h0;
-        end
-    end
+begin 
+    S_rd_rev_data_en <= S_data_fifo_rden;
 end
 
 always @(posedge I_sys_clk)
 begin
-    if(~S_clk_div2 && S_rd_rev_flg && (S_rd_rev_cnt == S_rd_rev_num))
+    if(S_rd_rev_flg && S_rd_rev_data_en)
     begin
         S_para_fifo_rden <= 1'b1;
     end
@@ -179,28 +197,71 @@ end
 
 always @(posedge I_sys_clk)
 begin
-    S_data_fifo_rden_d1 <= S_data_fifo_rden   ;
-    S_data_fifo_rden_d2 <= S_data_fifo_rden_d1;
-    S_data_fifo_rden_d3 <= S_data_fifo_rden_d2;
+    S_rd_rev_pos <= S_rd_rev_flg && S_rd_rev_data_en;
 end
 
 always @(posedge I_sys_clk)
 begin
-    S_ram_wr_en   <= S_data_fifo_rden_d3; 
-    S_ram_wr_data <= S_rd_rev_data      ;
+    if(S_rd_rev_pos)
+    begin
+        S_rd_rev_valid <= 1'b1;
+        S_rd_rev_cnt   <= 5'h1;
+    end
+    else if(S_rd_rev_end)
+    begin
+        S_rd_rev_valid <= 1'b0;
+        S_rd_rev_cnt   <= 5'h0;
+    end
+    else if(S_rd_rev_valid && S_rd_rev_data_en_d1)
+    begin
+        S_rd_rev_valid <= S_rd_rev_valid;
+        S_rd_rev_cnt   <= S_rd_rev_cnt + 5'h1;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_rd_rev_valid && S_rd_rev_data_en_d2 && (S_rd_rev_cnt == S_rd_rev_num))
+    begin
+        S_rd_rev_end <= 1'b1;
+    end
+    else
+    begin
+        S_rd_rev_end <= 1'b0;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    S_rd_rev_pos_d1 <= S_rd_rev_pos   ;
+    S_rd_rev_end_d1 <= S_rd_rev_end   ;
+    S_rd_rev_end_d2 <= S_rd_rev_end_d1;
+    S_rd_rev_cnt_d1 <= S_rd_rev_cnt   ;
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_rd_rev_end_d1)
+    begin
+        S_ram_base_addr <= S_ram_base_addr + S_rd_rev_cnt_d1;
+    end
+    else if(S_rd_rev_pos_d1)
+    begin
+        S_ram_base_addr <= S_ram_wr_addr;
+    end
 end
 
 always @(posedge I_sys_clk)
 begin
     if(S_ram_wr_en)
     begin
-        if((S_rd_rev_cnt == 5'h0) && (S_rd_rev_cnt < S_rd_rev_num))
+        if(S_rd_rev_valid)
         begin
-            S_ram_wr_addr <= S_ram_wr_addr + S_rd_rev_num - S_rd_rev_cnt;
+            S_ram_wr_addr <= S_ram_base_addr + {5'h0,S_rd_rev_num - S_rd_rev_cnt} + 10'h1;
         end
-        else if((S_rd_rev_cnt != 5'h0) && (S_rd_rev_cnt < S_rd_rev_num))
+        else if(S_rd_rev_end_d2)
         begin
-            S_ram_wr_addr <= S_ram_wr_addr - S_rd_rev_cnt;
+            S_ram_wr_addr <= S_ram_base_addr + 10'h1;
         end
         else
         begin
@@ -208,6 +269,171 @@ begin
         end
     end
 end
+
+always @(posedge I_sys_clk)
+begin
+    S_rd_rev_data_en_d1 <= S_rd_rev_data_en   ;
+    S_rd_rev_data_en_d2 <= S_rd_rev_data_en_d1;
+    S_rd_rev_data_en_d3 <= S_rd_rev_data_en_d2;
+    S_rd_rev_data_en_d4 <= S_rd_rev_data_en_d3;
+end
+
+always @(posedge I_sys_clk)
+begin
+    S_rd_rev_data_d1    <= S_rd_rev_data      ;
+    S_rd_rev_data_d2    <= S_rd_rev_data_d1   ;
+    S_rd_rev_data_d3    <= S_rd_rev_data_d2   ;
+    S_rd_rev_data_d4    <= S_rd_rev_data_d3   ;
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_rd_rev_data_en_d4)
+    begin
+        S_ram_wr_en   <= 1'b1; 
+        S_ram_wr_data <= S_rd_rev_data_d4;
+    end
+    else
+    begin
+        S_ram_wr_en   <= 1'b0;
+        S_ram_wr_data <= S_ram_wr_data;
+    end
+end
+
+blk_mem_1k_8bit  blk_mem_1k_8bit_inst (
+  .clka  (I_sys_clk             ), // input wire clka
+  .rsta  (I_sys_rst             ), // input wire rsta
+  .wea   (S_ram_wr_en           ), // input wire [0 : 0] wea
+  .ena   (1'b1                  ), // input wire [0 : 0] ena
+  .addra (S_ram_wr_addr         ), // input wire [9 : 0] addra
+  .dina  (S_ram_wr_data         ), // input wire [7 : 0] dina
+  .douta (                      ), // output wire [7 : 0] douta
+  .clkb  (I_sys_clk             ), // input wire clkb
+  .rstb  (I_sys_rst             ), // input wire rstb
+  .web   (1'b0                  ), // input wire [0 : 0] web
+  .enb   (S_ram_rd_en           ), // input wire [0 : 0] enb
+  .addrb (S_ram_rd_addr         ), // input wire [9 : 0] addrb
+  .dinb  (23'h0                 ), // input wire [7 : 0] dinb
+  .doutb (S_ram_rd_data         )  // output wire [7 : 0] doutb
+);
+
+//read process wrong ,waiting for next step
+always @(posedge I_sys_clk)
+begin
+    if(S_ram_rd_addr >= 10'h14f)
+    begin
+        S_ram_rd_ready <= 1'b0;
+    end
+    else if(S_ram_wr_addr > 10'h14f)
+    begin
+        S_ram_rd_ready <= 1'b1;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_ram_rd_ready)
+    begin
+        S_ram_rd_en <= ~S_ram_rd_en; 
+    end
+    else
+    begin
+        S_ram_rd_en <= 1'b0;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_ram_rd_en)
+    begin
+        S_ram_rd_addr <= S_ram_rd_addr + 10'h1;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    S_ram_rd_en_d1 <= S_ram_rd_en   ;
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_ram_rd_en_d1)
+    begin
+        O_payload_data_en <= 1'b1;
+        O_payload_data    <= S_ram_rd_data;
+    end
+    else
+    begin
+        O_payload_data_en <= 1'b0;
+        O_payload_data    <= O_payload_data;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    S_state_clr <= I_state_clr;
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_state_clr)
+    begin
+        S_data_fifo_wr_cnt <= 16'h0;
+    end
+    else if(S_data_fifo_wren)
+    begin
+        S_data_fifo_wr_cnt <= S_data_fifo_wr_cnt + 16'h1;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_state_clr)
+    begin
+        S_data_fifo_rd_cnt <= 16'h0;
+    end
+    else if(S_data_fifo_rden)
+    begin
+        S_data_fifo_rd_cnt <= S_data_fifo_rd_cnt + 16'h1;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_state_clr)
+    begin
+        S_para_fifo_wr_cnt <= 16'h0;
+    end
+    else if(S_para_fifo_wren)
+    begin
+        S_para_fifo_wr_cnt <= S_para_fifo_wr_cnt + 16'h1;
+    end
+end
+
+always @(posedge I_sys_clk)
+begin
+    if(S_state_clr)
+    begin
+        S_para_fifo_rd_cnt <= 16'h0;
+    end
+    else if(S_para_fifo_rden)
+    begin
+        S_para_fifo_rd_cnt <= S_para_fifo_rd_cnt + 16'h1;
+    end
+end
+
+//*  .--,       .--,
+//* ( (  \.---./  ) )
+//*  '.__/o   o\__.'
+//*     {=  ^  =}
+//*      >  -  <
+//*     /       \
+//*    //       \\
+//*   //|   .   |\\
+//*   "'\       /'"_.-~^`'-.
+//*      \  _  /--'         `
+//*    ___)( )(___
+//*   (((__) (__)))  
 
 
 
---
+++ runsim/run_qst_vrd_nuvm_vvd1602_nhls_tc1_lzw/flist.f
@@ -0,0 +1,17 @@
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/ipsim/blk_mem_1024_29_sim_netlist.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/ipsim/blk_mem_16k_23_sim_netlist.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/ipsim/blk_mem_32_8bit_sim_netlist.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/ipsim/fifo_1k_5bit_sim_netlist.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/ipsim/fifo_1k_9bit_sim_netlist.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/ipsim/fifo_8k_14bit_sim_netlist.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_backward_byte_reverse.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_backward_data_recover.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_backward_decompress.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_backward_dictionary.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_backward_dictionary_consult.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_forward_compress.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_forward_dictionary.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_forward_framer.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/lzw_forward_prepare.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/rtl/sync_reset.v
+/media/cqiu/e/work/prj/AIPrj/sim/sim_fxx/tb/btc/qst_vrd_nuvm_vvd1602_nhls_tc1_lzw/tb_lzw_top.v
